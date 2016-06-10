238f6fd6f9f4f45dde43a93e9c5c73c6dee2347b
Nokius <jupaja@gmx.net>
2016-06-10 18:34:01 +0000

    [bluetooth] get proper MAC address for BT device in Settings -> About
    Change-Id: I0b87058af9654afe4dce01596e325c39955fac66

--- sparse/usr/bin/droid/droid-hcismd-up.sh
+++ sparse/usr/bin/droid/droid-hcismd-up.sh
@@ -5,10 +5,20 @@
 # not work, so we loop.
 MAXTRIES=15
 
-seq 1 $MAXTRIES | while read i ; do
+#setprop bluetooth.hciattach true
+i=1
+while [ ! $i -gt $MAXTRIES ]  ; do
     echo 1 > /sys/module/hci_smd/parameters/hcismd_set
     if [ -e /sys/class/bluetooth/hci0 ] ; then
         # found hci0, exit successfully
+        echo 0 > /sys/module/hci_smd/parameters/hcismd_set
+        bt_mac=$(/system/bin/hci_qcomm_init -e -p 2 -P 2 -d /dev/ttyHSL0 2>1 | grep -oP '([0-9a-f]{2}:){5}([0-9a-f]{2})')
+        echo "BT MAC: $bt_mac"
+        if [ ! -z "$bt_mac" ] ; then
+            echo $bt_mac > /var/lib/bluetooth/board-address
+            echo "BT MAC: $bt_mac"
+        fi
+        echo 1 > /sys/module/hci_smd/parameters/hcismd_set
         exit 0
     fi
     sleep 1
